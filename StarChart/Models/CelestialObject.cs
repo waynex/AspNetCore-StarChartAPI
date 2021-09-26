1258a4f04e8b13cd7868186bb3797bf185f40ec9
Wayne Grate <waynexgm@gmail.com>
2021-09-26 17:35:04 -0500

    Added Celestial Object class

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        public int? OrbitedObjectId { get; set; }
+
+        //Satellite property
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
